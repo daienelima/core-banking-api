460200efad45449647e5bdfebc1395494db83e1e
daienelima <daiene.m17@gmail.com>
2026-04-16 14:15:47 -0300

    feat: adiciona CI com GitHub Actions

--- src/test/java/core/banking/api/CoreBankingApiApplicationTests.java
+++ src/test/java/core/banking/api/CoreBankingApiApplicationTests.java
@@ -2,8 +2,10 @@ package core.banking.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CoreBankingApiApplicationTests {
 
 	@Test
